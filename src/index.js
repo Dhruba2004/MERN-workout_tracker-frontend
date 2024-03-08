964b106b039751761622afd9400c1610c1cc2aaf
Dhruba2004 <dhrubaduttabanik8@gmail.com>
2024-03-09 02:15:22 +0530

    fix : some minor changes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { WorkoutsContextProvider } from './context/WorkoutContext'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import { AuthContextProvider } from './context/AuthContext'
 
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
